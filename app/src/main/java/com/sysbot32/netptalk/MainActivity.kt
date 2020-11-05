6fd786f3e462a0713ca8b722d967766b7a58f568
Seokjin Yoon <sjy5386@gmail.com>
2020-11-05 14:00:10 +0900

    Add view binding

--- app/src/main/java/com/sysbot32/netptalk/MainActivity.kt
+++ app/src/main/java/com/sysbot32/netptalk/MainActivity.kt
@@ -2,10 +2,14 @@ package com.sysbot32.netptalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sysbot32.netptalk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
