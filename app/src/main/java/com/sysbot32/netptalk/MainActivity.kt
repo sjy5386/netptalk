f2206378c9f6a023c2fce1f2bf80181116bfc551
Seokjin Yoon <sjy5386@gmail.com>
2020-11-12 15:54:55 +0900

    Update MainActivity

--- app/src/main/java/com/sysbot32/netptalk/MainActivity.kt
+++ app/src/main/java/com/sysbot32/netptalk/MainActivity.kt
@@ -7,12 +7,17 @@ import com.sysbot32.netptalk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var client: Client
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        startActivity(Intent(this, LoginActivity::class.java))
+        client = Client.getInstance()
+
+        if (!client.isConnected) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
