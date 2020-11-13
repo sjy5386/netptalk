a6c6fc53f21666c0e3d3598171a22c629153b529
Seokjin Yoon <sjy5386@gmail.com>
2020-11-13 12:40:31 +0900

    Add menu_main

--- app/src/main/java/com/sysbot32/netptalk/MainActivity.kt
+++ app/src/main/java/com/sysbot32/netptalk/MainActivity.kt
@@ -2,6 +2,8 @@ package com.sysbot32.netptalk
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.sysbot32.netptalk.databinding.ActivityMainBinding
 
@@ -19,5 +21,18 @@ class MainActivity : AppCompatActivity() {
         if (!client.isConnected) {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menuLogout) {
+            client.disconnect()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
