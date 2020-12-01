441228f5b1fb44a75a1c2c6bcb8be019b020e6df
Seokjin Yoon <sjy5386@gmail.com>
2020-12-01 13:59:26 +0900

    Add data class ChatFile

--- app/src/main/java/com/sysbot32/netptalk/File.kt
+++ app/src/main/java/com/sysbot32/netptalk/File.kt
@@ -1,8 +1,39 @@
 package com.sysbot32.netptalk
 
+import android.util.Base64
+import org.json.JSONObject
 import java.io.ByteArrayOutputStream
 import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
+
+data class ChatFile(val filename: String, val data: ByteArray) {
+    constructor(jsonObject: JSONObject) : this(
+        jsonObject.getString("filename"),
+        Base64.decode(jsonObject.getString("data"), 0)
+    )
+
+    fun toJSONObject(): JSONObject = JSONObject()
+        .put("filename", filename)
+        .put("data", Base64.encodeToString(data, 0))
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ChatFile
+
+        if (filename != other.filename) return false
+        if (!data.contentEquals(other.data)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = filename.hashCode()
+        result = 31 * result + data.contentHashCode()
+        return result
+    }
+}
 
 fun compress(data: ByteArray): ByteArray {
     val byteArrayOutputStream = ByteArrayOutputStream()
